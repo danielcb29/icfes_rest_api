5b00b1a48a18bdcf3c58852116c715663c2ae361
Daniel Correa <danielc.web@gmail.com>
2016-10-29 13:17:37 -0500

    Settings con nuevo static config

--- icfes_api/settings.py
+++ icfes_api/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '..', "icfes_api/static"),
+    os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'icfes_api/static_collected')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
